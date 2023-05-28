33a9bfb70804b2555f922d01ad20a7db4c524297
SUMON <belalhossain22000@gmail.comgit config --global user.name>
2023-05-28 14:47:57 +0600

    added user cart

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 //mongo db connection
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dp3om9f.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,6 +29,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const menuCollection = client.db("bistroDb").collection("menu");
+    const cartsCollection = client.db("bistroDb").collection("carts");
 
     app.get("/menu", async (req, res) => {
       try {
@@ -41,6 +42,34 @@ async function run() {
         console.error("Error retrieving menu items: ", err);
         res.status(500).json({ error: "Internal Server Error" });
       }
+    });
+
+    //get api for get by user email
+
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //post api for add to cart
+    app.post("/carts", async (req, res) => {
+      const item = req.body;
+      const result = await cartsCollection.insertOne(item);
+      res.send(result);
+    });
+
+    //delete cart item by id
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result=await cartsCollection.deleteOne(query);
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
